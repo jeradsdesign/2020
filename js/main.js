3b6f5fc6b073ba8fca6dd906768a6f032b32c793
jeradsdesign <jerad.horning@gmail.com>
2020-05-21 16:18:59 -0400

    Added some animations and fixed some spacing

--- js/main.js
+++ js/main.js
@@ -21,6 +21,57 @@ const navSlide = () => {
 	});
 }
 
+// on page load actions
+// const elementsOnload = document.querySelector(".action-onload");
+// document.addEventListener("DOMContentLoaded", ()=>{
+// 	elementsOnload.classList.add("activate");
+// });
+
+// slide in on Scroll
+function debounce(func, wait = 20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+}
+const elements = document.querySelectorAll(".action");
+const triggerMultiplier = 1.5;
+const windowHeight = window.innerHeight - 35;
+
+scrollAppear = () => {
+  elements.forEach(element => {
+    //   get each element's distance from top of screen & computed height
+    let positionFromTop = element.getBoundingClientRect().top;
+    let elementHeight = parseInt(window.getComputedStyle(element).height);
+
+    // trigger animation on scroll down
+    if (positionFromTop < windowHeight) {
+      element.classList.add("activate");
+    }
+
+    // re-hide elements after leaving the screen
+    if (
+      positionFromTop > windowHeight * triggerMultiplier ||
+      positionFromTop < -elementHeight
+    ) {
+      // element.classList.remove("activate");
+    }
+  });
+};
+// run function when scrolling
+window.addEventListener("scroll", debounce(scrollAppear));
+
+// end of slide in on Scroll
+
+
 const app = ()=>{
 	navSlide();
 }
