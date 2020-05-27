a668d8f67c04ba3c6ef311872fe19bbb6538340f
jeradsdesign <jerad.horning@gmail.com>
2020-05-27 16:14:34 -0400

    Added blog entries and styling

--- js/main.js
+++ js/main.js
@@ -71,6 +71,38 @@ window.addEventListener("scroll", debounce(scrollAppear));
 
 // end of slide in on Scroll
 
+//smooth scroll
+function smoothScroll(target, duration){
+	var target = document.querySelector(target);
+	var targetPosition = target.getBoundingClientRect().top;
+	var startPosition = window.pageYOffset;
+	var distance = targetPosition - startPosition;
+	var startTime = null;
+
+	function animation(currentTime){
+		if(startTime === null) startTime = currentTime;
+		var timeElapsed = currentTime - startTime;
+		var run = ease(timeElapsed, startPosition, distance, duration);
+		window.scrollTo(0,run);
+		if(timeElapsed < duration) requestAnimationFrame(animation);
+	}
+
+	  function ease(t, b, c, d) {
+	    t /= d / 2;
+	    if (t < 1) return c / 2 * t * t + b;
+	    t--;
+	    return -c / 2 * (t * (t - 2) - 1) + b;
+	  }
+
+	requestAnimationFrame(animation);
+}
+
+var scrollElement = document.querySelector('.scrollLink');
+
+scrollElement.addEventListener('click', function(){
+	smoothScroll('.scrollTarget', 1500);
+});
+
 
 const app = ()=>{
 	navSlide();
